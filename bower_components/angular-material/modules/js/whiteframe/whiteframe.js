cc24cfdeccab34e9abac4bb82eba3bb0c6689688
Martin Henry <emailnitram@gmail.com>
2014-12-03 22:55:56 -0800

    initial commit

---
+++ bower_components/angular-material/modules/js/whiteframe/whiteframe.js
@@ -0,0 +1,15 @@
+/*!
+ * Angular Material Design
+ * https://github.com/angular/material
+ * @license MIT
+ * v0.6.0-master-764a723
+ */
+(function() {
+'use strict';
+
+/**
+ * @ngdoc module
+ * @name material.components.whiteframe
+ */
+angular.module('material.components.whiteframe', []);
+})();
